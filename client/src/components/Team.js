e59f3dd851efb976546c981aaec464719e941f7c
John <jrmint46@gmail.com>
2020-09-13 21:15:48 -0400

    Made member names in teams link to user/:id

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -1,47 +1,56 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
 const Team = ({ teamName, teamId, members, updateTeams }) => {
   const [teamMembers, setTeamMembers] = useState(members);
 
-  const handleMemberClick = e => {
+  const handleMemberClick = (e) => {
     e.preventDefault();
 
-    const newMembers = teamMembers.filter(member => member.id !== e.target.id);
+    const newMembers = teamMembers.filter(
+      (member) => member.id !== e.target.id
+    );
     console.log(newMembers);
     axios.put(`http://localhost:5000/team/${teamId}`, {
-      members: newMembers
-    })
+      members: newMembers,
+    });
     // setTeamMembers(newMembers);
     const newTeam = {
       _id: teamId,
       teamName,
-      members: newMembers
-    }
+      members: newMembers,
+    };
     console.log(newTeam);
     updateTeams(newTeam);
-  }
+  };
 
   useEffect(() => {
     setTeamMembers(members);
-  }, [members])
+  }, [members]);
 
   return (
-    <div className="team-container">
+    <div className='team-container'>
       <ul>
         <li>{teamName}</li>
-        {teamMembers.map(member =>
-          <ul className="team-member" key={member.id}>
-            <button onClick={handleMemberClick} id={member.id}>Remove Member</button>
-            <li>{member.name}</li>
+        {teamMembers.map((member) => (
+          <ul className='team-member' key={member.id}>
+            <button onClick={handleMemberClick} id={member.id}>
+              Remove Member
+            </button>
+            <li>
+              <Link to={{ pathname: `/user/${member.id}` }}>{member.name}</Link>
+            </li>
             <li>{member.email}</li>
-            <li>{member.onlineSales ? "Online Sale Member" : "Floor Sale Member"}</li>
+            <li>
+              {member.onlineSales ? 'Online Sale Member' : 'Floor Sale Member'}
+            </li>
           </ul>
-        )}
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
